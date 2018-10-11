33028f1577546bc59a112313836c575bc673a33e
lights <lights@tutanota.de>
2018-10-11 15:16:27 -0400

    add migrations branch for forums

---
+++ versions/29040202cb0d_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 29040202cb0d
+Revises: 
+Create Date: 2018-10-11 15:15:13.888671
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '29040202cb0d'
+down_revision = None
+branch_labels = ('forums',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
