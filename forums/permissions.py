2d12e0bbd85a02970bf3c8eb3e5c4003df13455d
lights <lights@tutanota.de>
2018-10-16 15:23:44 -0400

    turn permissions into enum

---
+++ forums/permissions.py
@@ -0,0 +1,16 @@
+from core.mixins import Permission
+
+
+class ForumPermissions(Permission):
+    VIEW = 'forums_view'
+    CREATE_POST = 'forums_posts_create'
+    MODIFY_POST = 'forums_posts_modify'
+    DOUBLE_POST = 'forums_posts_double'
+    POST_IN_LOCKED = 'forums_posts_in_locked'
+    CREATE_THREAD = 'forums_threads_create'
+    MODIFY_THREAD = 'forums_threads_modify'
+    MODIFY_THREAD_ADVANCED = 'forums_threads_modify_advanced'
+    MODIFY_SUBSCRIPTIONS = 'forums_subscriptions_modify'
+    MODIFY_FORUMS = 'forums_forums_modify'
+    MODIFY_POLLS = 'forums_polls_vote'
+    VIEW_SUBSCRIPTIONS = 'forums_view_subscriptions'
